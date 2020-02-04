9ddf2f36cb0e86728662786c490dc8c0e7e61ea2
Michael Hammer <mike.p.hammer@gmail.com>
2020-02-04 11:35:30 -0500

    starting to make helm vendor changes

---
+++ pkg/util/helm.go
@@ -0,0 +1,52 @@
+/*
+Copyright (C) 2020 Synopsys, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements. See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership. The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package util
+
+import (
+	"os"
+	"os/exec"
+)
+
+func HelmIsInPath() bool {
+	if !fileExists("helm") {
+		return "helm not installed"
+	}
+	
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func HelmCreate() (string, error) {
+	args := []string{"install"}
+	cmd := exec.Command("helm", args...)
+	stdoutErr, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(stdoutErr), err
+	}
+	return string(stdoutErr), nil
+}
